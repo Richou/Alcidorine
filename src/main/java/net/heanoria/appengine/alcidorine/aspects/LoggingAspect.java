26c36c041284f392f9906475a7b990dc9c3dae53
Richou <Richard@Lutty>
2015-01-03 01:23:42 +0100

    Style en mode device, logginaspect modifié et ajout du plugin sonar.

--- src/main/java/net/heanoria/appengine/alcidorine/aspects/LoggingAspect.java
+++ src/main/java/net/heanoria/appengine/alcidorine/aspects/LoggingAspect.java
@@ -1,10 +1,12 @@
 package net.heanoria.appengine.alcidorine.aspects;
 
+import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 
 @Aspect
 public class LoggingAspect {
@@ -13,9 +15,11 @@ public class LoggingAspect {
 	
 	@Around("@annotation(net.heanoria.appengine.alcidorine.aspects.Logging)")
 	public Object loggingAround(ProceedingJoinPoint pjp) throws Throwable {
-		logger.info("Before aspect");
+		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+		Method method = methodSignature.getMethod();
+		logger.info("Entering " + method.getName() + "() method");
 		Object ret = pjp.proceed();
-		logger.info("After aspect");
+		logger.info("Exiting " + method.getName() + "() method");
 		return ret;
 	}
 }
