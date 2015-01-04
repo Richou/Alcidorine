67458a51d183863957a998aee758026f911e555a
Richou <Richard@Lutty>
2015-01-04 05:40:46 +0100

    Rollback exception.

--- src/main/java/net/heanoria/appengine/alcidorine/aspects/LoggingAspect.java
+++ src/main/java/net/heanoria/appengine/alcidorine/aspects/LoggingAspect.java
@@ -3,8 +3,6 @@ package net.heanoria.appengine.alcidorine.aspects;
 import java.lang.reflect.Method;
 import java.util.logging.Logger;
 
-import net.heanoria.appengine.alcidorine.exceptions.AspectException;
-
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,16 +14,12 @@ public class LoggingAspect {
     private static final Logger LOGGER = Logger.getLogger(LoggingAspect.class.getName());
     
     @Around("@annotation(net.heanoria.appengine.alcidorine.aspects.Logging)")
-    public Object loggingAround(ProceedingJoinPoint pjp) {
-    	try{
-    		MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
-    		Method method = methodSignature.getMethod();
-    		LOGGER.info("Entering " + method.getName() + "() method");
-    		Object ret = pjp.proceed();
-    		LOGGER.info("Exiting " + method.getName() + "() method");
-    		return ret;
-    	} catch(Throwable t) {
-    		throw new AspectException(t.getMessage());
-    	}
+    public Object loggingAround(ProceedingJoinPoint pjp) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        Method method = methodSignature.getMethod();
+        LOGGER.info("Entering " + method.getName() + "() method");
+        Object ret = pjp.proceed();
+        LOGGER.info("Exiting " + method.getName() + "() method");
+        return ret;
     }
 }
