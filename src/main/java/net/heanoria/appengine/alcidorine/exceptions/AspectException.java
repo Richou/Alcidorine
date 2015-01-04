9fb48bb798384d7d83c62b591db236674f867df6
Richou <Richard@Lutty>
2015-01-04 05:25:21 +0100

    Ajout une classe d'exception.

---
+++ src/main/java/net/heanoria/appengine/alcidorine/exceptions/AspectException.java
@@ -0,0 +1,14 @@
+package net.heanoria.appengine.alcidorine.exceptions;
+
+public class AspectException extends RuntimeException{
+
+	private static final long serialVersionUID = 6966607742102747544L;
+
+	public AspectException() {
+		super();
+	}
+	
+	public AspectException(String message) {
+		super(message);
+	}
+}
